6d4eccbb96e2b1cbfad9770cb56174a12d722858
DavidSGS <davidgarcia200124@gmail.com>
2023-11-01 23:21:35 -0500

    Boton

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -2,7 +2,11 @@
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			animation: {
+				'spin_slow': 'spin 4s linear infinite',
+			  }
+		},
 	},
 	plugins: [],
 }
