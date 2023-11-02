a43d4752e8807ddfec7e8af35f63f3d8e14688e8
DavidSGS <davidgarcia200124@gmail.com>
2023-11-02 03:41:55 -0500

    Creacion de las cartas, creacion de una ruta dinamica haciendo uso de la funcion getStaticPaths, creacion del otro fetch para buscar por id, creacion de los typos de la api

---
+++ src/lib/FetchData.ts
@@ -0,0 +1,28 @@
+import { type Doc, type API } from "../types/api";
+
+export const getDataByID = async  ({id}: {id:string}) => { 
+  const res = await fetch(`https://api.spacexdata.com/v5/launches/${id}`)
+  const lanzamientos = (await res.json()) as Doc;   
+  return lanzamientos;
+} 
+
+export const getData = async  () => { 
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "asc",
+        },
+        limit: 12,
+      },
+    }),
+  });
+  
+  const { docs: lanzamientos } = (await res.json()) as API;   
+  return lanzamientos;
+}
