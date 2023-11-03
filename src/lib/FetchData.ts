65d445af2fd4ef251a53f9f75126c49a982160b3
DavidSGS <davidgarcia200124@gmail.com>
2023-11-03 15:19:52 -0500

    Se agrego la seccion de ultimos lanzamientos

--- src/lib/FetchData.ts
+++ src/lib/FetchData.ts
@@ -26,3 +26,24 @@ export const getData = async  () => {
   const { docs: lanzamientos } = (await res.json()) as API;   
   return lanzamientos;
 }
+
+export const getDataFirst = async  (startPosition: number) => { 
+  const res = await fetch("https://api.spacexdata.com/v5/launches/query", {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify({
+      query: {},
+      options: {
+        sort: {
+          date_unix: "desc",
+        },
+        limit: startPosition + 12,
+      },
+    }),
+  });
+  
+  const { docs: lanzamientos } = (await res.json()) as API;   
+  return lanzamientos.slice(startPosition, startPosition + 12);
+}
